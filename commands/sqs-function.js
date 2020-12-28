616341037fc582d5edfe5e58f1272a45ad5ae5f4
Aleksander Hansson <hello@ahansson.com>
2020-12-27 23:38:47 -0800

    make plugin functional

--- commands/sqs-function.js
+++ commands/sqs-function.js
@@ -1,4 +1,4 @@
-const featureName = 'sqs triggered lambda';
+const featureName = "sqsfunction";
 
 module.exports = {
   name: featureName,
@@ -8,24 +8,24 @@ module.exports = {
         const { run } = require(`./${featureName}/${context.parameters.first}`);
         return run(context);
       } catch (e) {
-        context.print.error('Command not found');
+        context.print.error("Command not found");
       }
     }
     const header = `amplify ${featureName} <subcommand>`;
 
     const commands = [
       {
-        name: 'add',
-        description: `Takes you through a CLI flow to add a ${featureName} resource to your local backend`,
+        name: "add",
+        description: `Takes you through a CLI flow to add a ${featureName} resource to your local backend`
       },
       {
-        name: 'remove',
-        description: `Removes ${featureName} resource from your local backend and will remove them on amplify push`,
+        name: "remove",
+        description: `Removes ${featureName} resource from your local backend and will remove them on amplify push`
       }
     ];
 
     context.amplify.showHelp(header, commands);
 
-    context.print.info('');
-  },
+    context.print.info("");
+  }
 };
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+const category = "sqs-function";
+const path = require("path");
+
 async function executeAmplifyCommand(context) {
-    let commandPath = path.normalize(path.join(__dirname, 'commands'));
-    if (context.input.command === 'help') {
-      commandPath = path.join(commandPath, category);
-    } else {
-      commandPath = path.join(commandPath, category, context.input.command);
-    }
-    
-    const commandModule = require(commandPath);
-    await commandModule.run(context);
+  let commandPath = path.normalize(path.join(__dirname, "commands"));
+  if (context.input.command === "help") {
+    commandPath = path.join(commandPath, category);
+  } else {
+    commandPath = path.join(commandPath, category, context.input.command);
   }
 
-  module.exports = {
-    executeAmplifyCommand,
-  };
+  console.log(context);
+  console.log(commandPath);
+
+  const commandModule = require(commandPath);
+  console.log(commandModule);
+  await commandModule.run(context);
+}
+
+module.exports = {
+  executeAmplifyCommand
+};
